55c6c6443ab5d3d615a15fe4b11a5592dbb60c6a
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2025-03-10 16:45:51 +0200

    Highlights changes

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -7,7 +7,7 @@ import VideoCarousel from "./VideoCarousel";
 const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.5 });
   }, []);
 
   return (
@@ -17,16 +17,24 @@ const Highlights = () => {
     >
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
-          <h1 id="title" className="section-heading">
+          <h1 id="title" className="section-heading text-white">
             Experience the Future.
           </h1>
 
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
-              <a href="https://docs.multiversx.com/" target="_blank">
+              <a
+                href="https://docs.multiversx.com/"
+                className=" text-emerald-300"
+                target="_blank"
+              >
                 Explore the Tech
               </a>
-              <img src={rightImg} alt="right" className="ml-2" />
+              <img
+                src={rightImg}
+                alt="right"
+                className="ml-2  text-emerald-300"
+              />
             </p>
           </div>
         </div>
